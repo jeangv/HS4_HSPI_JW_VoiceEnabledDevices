20707fb9124323d75d7d5cc8c55268516355630b
Jean-Marie Vaneskahian <jean@vaneskahian.com>
2021-02-26 11:11:08 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSPI_JW_VoiceEnabledDevices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
